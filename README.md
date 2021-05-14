3c1fb77df380eb4edf0a3ece26ea381eff2ee208
Kirill Stytsenko <kirill@styts.com>
2021-05-14 08:37:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# tz-vanity
+
+Utility for generating a command for using `tezos-client` to generate a vanity key pair with capitalization variations on the prefix. This helps the generator find desired name quicker.
+
+> tz-vanity --seed tezos --name tezos
+tezos-client gen vanity keys tezos matching Tezos TEzos TeZos TEZos TezoS TEzoS TeZoS TEZoS --prefix
+
+Watch:
+
+> yarn dev
+
+Build:
+
+> yarn build
---
+++ src/index.ts
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+
+const validChars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'.split('')
+const validFirstChar = 'KLMNPQRSTUVWXYZabcdefghi'.split('')
+
+function perms (input: string) {
+  // From https://stackoverflow.com/questions/27992204/find-all-lowercase-and-uppercase-combinations-of-a-string-in-javascript
+  const letters = input.split("");
+  const permCount = 1 << input.length;
+  const perms = []
+
+  for (let perm = 0; perm < permCount; perm++) {
+    letters.reduce((perm, letter, i) => {
+      letters[i] = (perm & 1) ? letter.toUpperCase() : letter.toLowerCase();
+      return perm >> 1;
+    }, perm);
+
+    const result = letters.join("");
+    if (validFirstChar.includes(letters[0])) {
+      if(validWord(result)) {
+        perms.push(result)
+      }
+    }
+  }
+  return perms
+}
+
+function printCmd (name :string, x: string[]) {
+  const words = x.join(' ')
+  console.log(`tezos-client gen vanity keys ${name} matching ${words} --prefix`)
+}
+
+function validWord (word: string) {
+  const letter = word.split('')
+  for (var i = 0; i < letter.length; i++) {
+    var c = letter[i]
+    if(!validChars.includes(c)) {
+      return false
+    }
+  }
+  return true
+}
+
+function validateSeed (seed :string) {
+  const seeds = seed.split('')
+  for (var i = 0; i < seeds.length; i++) {
+    var c = seeds[i]
+    if(!validChars.includes(c)) {
+      throw new Error(`Seed word can not contain "${c}"`)
+    }
+  }
+}
+
+const seed = argv.seed
+validateSeed(seed)
+
+const words = perms(seed)
+printCmd(argv.name, words)
